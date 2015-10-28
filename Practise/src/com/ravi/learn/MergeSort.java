5d1d34a3c153247990c9ef194bb52bf50226758b
Raviteja <raviteja.rp@gmail.com>
2015-10-28 22:59:52 +0530

    Updated few more

---
+++ Practise/src/com/ravi/learn/MergeSort.java
@@ -0,0 +1,72 @@
+package com.ravi.learn;
+
+public class MergeSort {
+	private long[] numbers;
+	private long[] helper;
+	int counter = 0;
+	private int number;
+
+	public void sort(long[] values) {
+		this.numbers = values;
+		number = values.length;
+		this.helper = new long[number];
+		mergesort(0, number - 1);
+	}
+
+	private void mergesort(int low, int high) {
+		// check if low is smaller then high, if not then the array is sorted
+		if (low < high) {
+			// Get the index of the element which is in the middle
+			int middle = low + (high - low) / 2;
+			// Sort the left side of the array
+			mergesort(low, middle);
+			// Sort the right side of the array
+			mergesort(middle + 1, high);
+			// Combine them both
+			merge(low, middle, high);
+		} else if (high - low == 1) {
+			if (numbers[high] < numbers[low]) {
+				long l = numbers[high];
+				numbers[high] = numbers[low];
+				numbers[low] = l;
+				counter++;
+			}
+		}
+		System.out.println(counter);
+	}
+
+	private void merge(int low, int middle, int high) {
+
+		// Copy both parts into the helper array
+		for (int i = low; i <= high; i++) {
+			helper[i] = numbers[i];
+		}
+
+		int i = low;
+		int j = middle + 1;
+		int k = low;
+		// Copy the smallest values from either the left or the right side back
+		// to the original array
+		while (i <= middle && j <= high) {
+
+			if (helper[i] <= helper[j]) {
+				numbers[k] = helper[i];
+				i++;
+
+			} else {
+				numbers[k] = helper[j];
+				j++;
+				counter = counter + (middle - i);
+			}
+
+			k++;
+		}
+		// Copy the rest of the left side of the array into the target array
+		while (i <= middle) {
+			numbers[k] = helper[i];
+			k++;
+			i++;
+		}
+
+	}
+}
